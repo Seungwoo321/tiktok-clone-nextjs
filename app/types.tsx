2b666d582877d7a25c6765acba9c8c6c5e2a16fc
unknown <seungwoo321@gmail.com>
2024-05-18 21:36:48 +0900

    feat: add upload page

--- app/types.tsx
+++ app/types.tsx
@@ -4,6 +4,20 @@ export interface RandomUsers {
   image: string;
 }
 
+export interface Like {
+  id: string;
+  user_id: string;
+  post_id: string;
+}
+
+export interface Comment {
+  id: string;
+  user_id: string;
+  post_id: string;
+  text: string;
+  created_at: string;
+}
+
 export interface PostWithProfile {
   id: string;
   user_id: string;
@@ -15,6 +29,11 @@ export interface PostWithProfile {
     name: string;
     image: string;
   }
+}
+
+export interface UploadError {
+  type: string;
+  message: string;
 }
 
 ///////////////////////////////////
